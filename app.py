c72b52b7d349b6edcad0410f013733f0bd94e459
kosgeibenedict <kosgeibenedict3@gmail.com>
2019-01-25 13:39:56 +0300

    Created templates folder and static folder, also created register.html file in templates and styles.css in static and connected the rigister.html to external styles.css file

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, session, logging, url_for,redirect
+#from data import Articles
+#from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
